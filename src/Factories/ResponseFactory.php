cb0a2e19961aadccfa14d07dbdfb0d7c37e5184a
Daniel G Sims <danielsims@quickenloans.com>
2014-03-18 21:35:04 -0400

    Implemented more functions in the HTTP response factory

--- src/Factories/ResponseFactory.php
+++ src/Factories/ResponseFactory.php
@@ -18,5 +18,65 @@ namespace Shale\Response\Factories
     {
       return $this->send("An unexpected error occurred: ". $message, 500, $headers);
     }
+
+    public function created($content, $headers = array())
+    {
+      return $this->send($content, 201, $headers);
+    }
+
+    public function accepted($content, $headers = array())
+    {
+      return $this->send($content, 202, $headers);
+    }
+
+    public function noContent($content, $headers = array())
+    {
+      return $this->send("", 202, $headers);
+    }
+
+    public function movedPermantently($content, $headers = array())
+    {
+      return $this->send($content, 301, $headers);
+    }
+
+    public function found($content, $headers = array())
+    {
+      return $this->send($content, 302, $headers);
+    }
+
+    public function seeOther($content, $headers = array())
+    {
+      return $this->send($content, 303, $headers);
+    }
+
+    public function badRequest($content, $headers = array())
+    {
+      return $this->send($content, 400, $headers);
+    }
+
+    public function unauthorized($content, $headers = array())
+    {
+      return $this->send($content, 401, $headers);
+    }
+
+    public function forbidden($content, $headers = array())
+    {
+      return $this->send($content, 403, $headers);
+    }
+
+    public function methodNotAllowed($content, $headers = array())
+    {
+      return $this->send($content, 405, $headers);
+    }
+
+    public function notAcceptable($content, $headers = array())
+    {
+      return $this->send($content, 406, $headers);
+    }
+
+    public function send($message, $status, array $headers = array())
+    {
+      return new Response($message, $status, $headers);
+    }
   }
 }
